cbb28040959587d1632e7c438c23e6333e11e749
Quan <quantrung286@gmail.com>
2022-01-29 21:39:48 +0700

    up

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -13,14 +13,46 @@ import { useRouter } from 'next/router'
 import { MdCancel } from 'react-icons/md'
 import Image from 'next/image'
 
+import { filterData, getFilterValues } from './../utils/filterData'
+
 const SearchFilters = () => {
-	const [filters, setFilters] = useState({})
+	const [filters, setFilters] = useState(filterData)
+	const router = useRouter()
+
+	const searchProperties = (filterValues) => {
+		const { query, pathname: path } = router
+
+		const values = getFilterValues(filterValues)
+
+		values.forEach((item) => {
+			query[item.name] = item.value
+		})
+
+		router.push({ pathname: path, query })
+	}
 
 	return (
-    <Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
-		
+		<Flex bg='gray.100' p='4' justifyContent='center' flexWrap='wrap'>
+			{filters.map((filter) => (
+				<Box key={filter.queryName}>
+					<Select
+						placeholder={filter.placeholder}
+						w='fit-content'
+						p='2'
+						onChange={(e) =>
+							searchProperties({ [filter.queryName]: e.target.value })
+						}
+					>
+						{filter?.items?.map((item) => (
+							<option value={item.value} key={item.value}>
+								{item.name}
+							</option>
+						))}
+					</Select>
+				</Box>
+			))}
 		</Flex>
-  )
+	)
 }
 
 export default SearchFilters
