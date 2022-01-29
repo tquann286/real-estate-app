cb7483bdda1601cb778e39f27789704300e31141
Quan <quantrung286@gmail.com>
2022-01-29 17:33:51 +0700

    up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import React from 'react'
+import Router from 'next/router'
+import Head from 'next/head'
+import NProgress from 'nprogress'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<React.Fragment>
+			<Head></Head>
+			<ChakraProvider>
+				<Layout>
+					<Component {...pageProps} />
+				</Layout>
+			</ChakraProvider>
+		</React.Fragment>
+	)
 }
 
 export default MyApp
