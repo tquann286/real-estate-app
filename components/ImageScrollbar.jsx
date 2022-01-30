82ea135cf27bd4556093b4a3d4a527b62148d8c2
Quan <quantrung286@gmail.com>
2022-01-30 21:54:15 +0700

    up

--- components/ImageScrollbar.jsx
+++ components/ImageScrollbar.jsx
@@ -8,12 +8,13 @@ const LeftArrow = () => {
 	const { scrollPrev } = useContext(VisibilityContext)
 
 	return (
-		<Flex justifyContent='center' alignItems='center' marginLeft='1'>
+		<Flex justifyContent='center' alignItems='center' marginRight='1'>
 			<Icon
 				as={FaArrowAltCircleLeft}
-				onClick={scrollPrev}
+				onClick={() => scrollPrev()}
 				fontSize='2xl'
 				cursor='pointer'
+				d={['none', 'none', 'none', 'block']}
 			/>
 		</Flex>
 	)
@@ -23,37 +24,36 @@ const RightArrow = () => {
 	const { scrollNext } = useContext(VisibilityContext)
 
 	return (
-		<Flex justifyContent='center' alignItems='center' marginRight='1'>
+		<Flex justifyContent='center' alignItems='center' marginLeft='1'>
 			<Icon
 				as={FaArrowAltCircleRight}
-				onClick={scrollNext}
+				onClick={() => scrollNext()}
 				fontSize='2xl'
 				cursor='pointer'
+				d={['none', 'none', 'none', 'block']}
 			/>
 		</Flex>
 	)
 }
-
-const ImageScrollbar = ({ data }) => (
-	<ScrollMenu
-		LeftArrow={LeftArrow}
-		RightArrow={RightArrow}
-		style={{ overflow: 'hidden' }}
-	>
-		{data.map((item) => (
-			<Box key={item.id} width={910} itemID='item.id' overflow='hidden' p={1}>
-				<Image
-					alt='property'
-					placeholder='blur'
-					blurDataURL={item.url}
-					src={item.url}
-					width={1000}
-					height={500}
-          sizes='(max-width:500px) 100px, (max-width):1023px 400px, 1000px'
-				/>
-			</Box>
-		))}
-	</ScrollMenu>
-)
-
-export default ImageScrollbar
+export default function ImageSrollbar({ data }) {
+	return (
+		<ScrollMenu
+			LeftArrow={LeftArrow}
+			RightArrow={RightArrow}
+			style={{ overflow: 'hidden' }}
+		>
+			{data.map((item) => (
+				<Box key={item.id} width='910px' itemId={item.id} overflow='hidden' p='1'>
+					<Image
+						placeholder='blur'
+						blurDataURL={item.url}
+						src={item.url}
+						width={1000}
+						height={500}
+						sizes='(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px'
+					/>
+				</Box>
+			))}
+		</ScrollMenu>
+	)
+}
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,4 +1,5 @@
-import { Box, Flex, Spacer, Text, Avatar } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Box, Flex, Spacer, Text, Avatar, Button } from '@chakra-ui/react'
 import { FaBed, FaBath } from 'react-icons/fa'
 import { BsGridFill } from 'react-icons/bs'
 import { GoVerified } from 'react-icons/go'
@@ -6,7 +7,6 @@ import millify from 'millify'
 
 import { baseUrl, fetchApi } from '../../utils/fetchApi'
 import ImageScrollbar from '../../components/ImageScrollbar'
-
 
 const PropertyDetails = ({
 	propertyDetails: {
@@ -25,11 +25,64 @@ const PropertyDetails = ({
 		amenities,
 		photos,
 	},
-}) => (
-  <Box maxWidth={1000} margin='auto' p={4}>
-  {photos && <ImageScrollbar data={photos} />}
-  </Box>
-)
+}) => {
+	const [readMore, setReadMore] = useState(false)
+
+	const handleReadMore = () => {
+		setReadMore(!readMore)
+	}
+
+	return (
+		<Box maxWidth={1000} margin='auto' p={4}>
+			{photos && <ImageScrollbar data={photos} />}
+			<Box width='full' p={6}>
+				<Flex paddingTop='2' alignItems='center' justifyContent='space-between'>
+					<Flex alignItems='center'>
+						<Box paddingRight='3' color='green.400'>
+							{isVerified && <GoVerified />}
+						</Box>
+						<Text fontWeight='bold' fontSize='lg'>
+							AED {millify(price)}
+							{rentFrequency && `/${rentFrequency}`}
+						</Text>
+					</Flex>
+					<Box>
+						<Avatar size='sm' src={agency.logo.url} />
+					</Box>
+				</Flex>
+				<Flex
+					alignItems='center'
+					p='1'
+					justifyContent='space-between'
+					w='250px'
+					color='blue.400'
+				>
+					{rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)} sqft{' '}
+					<BsGridFill />
+				</Flex>
+				<Box marginTop={2}>
+					<Text fontSize='lg' marginBottom={2} fontWeight='bold'>
+						{title}
+					</Text>
+					<Text lineHeight={2} color='gray.600'>
+						{readMore
+							? `${description} `
+							: `${description.substring(0, 200)}... `}
+						<Text
+							onClick={handleReadMore}
+							display='inline-block'
+							color='blue.400'
+							cursor='pointer'
+						>
+							{readMore ? 'Show less' : 'Show more'}
+						</Text>
+					</Text>
+				</Box>
+				<Flex flexWrap='wrap'></Flex>
+			</Box>
+		</Box>
+	)
+}
 
 export default PropertyDetails
 
